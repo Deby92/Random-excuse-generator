16786d513f055fbba61c91766b8faf92ab3a3aa5
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-06-28 19:03:19 +0000

    martes

--- src/app.js
+++ src/app.js
@@ -9,3 +9,50 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+generateExcuse = () => {
+  let pronoun = ["A", "The", "My", "Our", "Your"];
+  let proIndex = Math.floor(Math.random() * pronoun.length);
+  let subject = ["rabbit", "racoon", "owl", "kangaroo", "cat", "bat"];
+  let subjIndex = Math.floor(Math.random() * subject.length);
+  let action = [
+    "run to",
+    "steps on my",
+    "broke my",
+    "ate my",
+    "burn my",
+    "threw my"
+  ];
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possetion = [
+    "skate",
+    "laptop",
+    "bed",
+    "pen",
+    "cup of coffee",
+    "door",
+    "food"
+  ];
+  let possetionIndex = Math.floor(Math.random() * possetion.length);
+  let where = [
+    "on the floor",
+    "at the kitchen",
+    "on my home",
+    "on the bathroom",
+    "on the yard",
+    "at the apartment"
+  ];
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  let excuse =
+    pronoun[proIndex] +
+    " " +
+    subject[subjIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possetion[possetionIndex] +
+    " " +
+    where[whereIndex];
+  documen.getElementById("excuse").innerHTML = excuse;
+};
